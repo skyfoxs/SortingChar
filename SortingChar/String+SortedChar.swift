1002dcae312c504a8d8c976028d4328188b076a5
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-20 22:44:59 +0700

    extend String with sortedChar

---
+++ SortingChar/String+SortedChar.swift
@@ -0,0 +1,15 @@
+//
+//  String+SortedChar.swift
+//  SortingChar
+//
+//  Created by Supakit Thanadittagorn on 8/20/2559 BE.
+//  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var sortedChar: String {
+        return "act"
+    }
+}
--- SortingCharTests/SortingCharTests.swift
+++ SortingCharTests/SortingCharTests.swift
@@ -20,17 +20,8 @@ class SortingCharTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testInputCatShouldBeAct(){
+        XCTAssertEqual("cat".sortedChar, "act", "cat should sorted to act")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
