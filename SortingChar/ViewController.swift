39deb31e2663311c0ee4db890f220624d83b172b
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-21 16:25:43 +0700

    Add UI test for sortingChar

--- SortingChar/ViewController.swift
+++ SortingChar/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var sortedCharLabel: UILabel!
+    @IBAction func sorting() {
+        sortedCharLabel.text = inputTextField.text?.sortedChar
+    }
 }
 
---
+++ SortingCharUITests/SortingCharUITests.swift
@@ -0,0 +1,41 @@
+//
+//  SortingCharUITests.swift
+//  SortingCharUITests
+//
+//  Created by Supakit Thanadittagorn on 8/21/2559 BE.
+//  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
+//
+
+import XCTest
+
+class SortingCharUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSortingChar(){
+        let app = XCUIApplication()
+
+        app.textFields["Input"].tap()
+        app.textFields["Input"].typeText("cat")
+
+        app.buttons["Sorting"].tap()
+        XCTAssertEqual(app.staticTexts["SortedChar"].label, "act")
+    }
+    
+}
